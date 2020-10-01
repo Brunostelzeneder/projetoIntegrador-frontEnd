9c6e439e64f059280fc6d1019c1ebd4baa74a3b6
Brunostelzeneder <52729196+Brunostelzeneder@users.noreply.github.com>
2020-10-01 09:28:38 -0300

    Projeto Iniciado

---
+++ camisetasOnTheGo/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
