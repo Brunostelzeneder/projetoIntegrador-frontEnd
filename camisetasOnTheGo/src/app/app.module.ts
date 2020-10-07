4590ff72934d5257944a7e58bf444cbf8cf4cde2
Brunostelzeneder <52729196+Brunostelzeneder@users.noreply.github.com>
2020-10-07 10:10:21 -0300

    Sobrenosatualizado

--- camisetasOnTheGo/src/app/app.module.ts
+++ camisetasOnTheGo/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { SobrenosComponent } from './sobrenos/sobrenos.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { HttpClientModule } from '@angular/common/http';
     FooterComponent,
     ContatoComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
+    SobrenosComponent
   ],
   imports: [
     BrowserModule,
